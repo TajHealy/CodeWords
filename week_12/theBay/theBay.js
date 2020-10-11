1effa58350962a70164ab8961230870e47674fc3
TajHealy <68985113+TajHealy@users.noreply.github.com>
2020-10-11 19:30:11 +1100

    Update theBay.js

--- week_12/theBay/theBay.js
+++ week_12/theBay/theBay.js
@@ -38,7 +38,7 @@ function setup() {
   var typesize = (map(width,300,1650,50,200));
   textSize (typesize*multiplier);
   spacesize = 50; //width of space between letters
-  linesize = 280;
+  linesize = 280*multiplier;
   
   
   song.play();
@@ -93,7 +93,7 @@ function makeCreep(){
   var typesize = (map(width,300,1650,50,200));
   textSize (typesize*multiplier);
   spacesize = 50; //width of space between letters
-  linesize = 280;
+  linesize = 280*multiplier;
   
   
    song.play();
